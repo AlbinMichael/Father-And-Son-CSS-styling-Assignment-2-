7a7570c4fbf3e7c26321b5bf68eb98f33b5dcaa2
AlbinMichael <134335972+AlbinMichael@users.noreply.github.com>
2023-06-11 20:11:40 -0400

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Father and Son - </title>
+  <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+  <header>
+    <h1>Father and Son - Yusuf Islam</h1>
+  </header>
+  <main>
+    <section>
+      <h2>Lyrics</h2>
+      <p>
+        It's not time to make a <span class="change">change</span><br>
+        Just <span class="relax">relax</span>, take it easy<br>
+        You're still <span class="young">young</span>, that's your fault<br>
+        There's so much you have to know<br><br>
+        Find a <span class="girl">girl</span>, settle down<br>
+        If you want, you can <span class="marry">marry</span><br>
+        <span class="Line1">Look at me, I am old, but I'm happy</span><br><br>
+        I was once like you are now<br>
+        And I know that it's not easy<br>
+        To be <span class="calm">calm</span> when you've found<br>
+        Something going on<br><br>
+        But take your <span class="time">time</span>, think a lot<br>
+        Why, think of everything you've got<br>
+        For you will still be here <span class="tomorrow">tomorrow</span><br>
+        <span class="Line2">But your dreams may not</span><br><br>
+        How can I try to <span class="explain">explain</span><br>
+        When I do, he turns away again<br>
+        It's always been the same, <span class="Old">same old story</span><br><br>
+        From the moment I could talk,<br>
+        I was ordered to <span class="listen">listen</span><br>
+        Now there's a way,<br> 
+        And I know that I have to <span class="goaway">go away</span><br><br>
+        <span class="Line3">I know, I have to go</span><br><br>
+        It's not time to make a <span class="change1">change</span><br>
+        Just <span class="sitdown">sit down</span>, take it slowly<br>
+        You're still <span class="young1">young</span>, that's your fault<br>
+        There's so much you have to go through<br><br>
+        Find a <span class="girl1">girl</span>, settle down<br>
+        If you want, you can <span class="marry1">marry</span><br>
+        <span class="Line4">Look at me, I am old, but I'm happy</span><br><br/>
+        All the times that I've cried<br>
+        Keeping all the things I knew inside<br>
+        <span class="ignore">It's hard, but it's harder to ignore it</span><br><br/>
+        If they were right, I'd agree<br>
+        But it's them they know, not me<br>
+        Now there's a way, and I know<br>
+        That I have to <span class="goaway1">go away</span><br><br>
+        <span class="Line5">I know, I have to go</span>
+        
+      </p>
+    </section>
+  </main>
+  <footer>
+    <p>© 2023 Albin Michael</p>
+  </footer>
+</body>
+</html>
